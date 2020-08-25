46a94125a7a4d2c5ae437f71fcd736b1e18e64dd
samadurm <samadurm@oregonstate.edu>
2020-08-24 18:47:33 -0700

    Added basic event listener function for submit button

---
+++ index.js
@@ -0,0 +1,8 @@
+function handleSubmit(){
+    console.log("Clicked the button!!")
+    var length = document.getElementById("length").value;
+}
+
+
+document.getElementById("submit").addEventListener("click", handleSubmit);
+
