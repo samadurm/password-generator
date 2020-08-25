1b1135e44b32ce9f0ae39f7510795a82c42448aa
samadurm <samadurm@oregonstate.edu>
2020-08-24 19:12:56 -0700

    generates simple ascii lowercase character password when submit is clicked

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function handleSubmit(){
-    console.log("Clicked the button!!")
     var length = document.getElementById("length").value;
+    var password = generatePassword(length);
+
+    document.getElementById("result").innerHTML = password;
 }
 
+function generatePassword(length){
+    var password="";
+    for(var i = 0; i < length; i++){
+        var randChar = randomASCII(97, 122);
+        password += randChar;
+    }
+    return password;
+}
+
+function randomASCII(min, max){
+    return String.fromCharCode(Math.floor(Math.random() * (max - min) + min));
+}
 
 document.getElementById("submit").addEventListener("click", handleSubmit);
 
