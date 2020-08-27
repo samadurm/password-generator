a55ea9cb2a4cc3434df920f6027fd2f1296485a6
samadurm <samadurm@oregonstate.edu>
2020-08-26 19:08:41 -0700

    Added function to copy generated password to the clipboard

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function handleSubmit(){
     var length = document.getElementById("length").value;
     var maxLen = document.getElementById("length").max;
     
-    if(length > maxLen){
+    if(length < maxLen){
         length = maxLen;
     }
     console.log("Length is " + length)
@@ -24,9 +24,29 @@ function randomASCII(min, max){
     return String.fromCharCode(Math.floor(Math.random() * (max - min) + min));
 }
 
-document.getElementById("submit").addEventListener("click", handleSubmit);
-
+/*
+    Function: copyToClipboard
+    Description: copies the password to the clipboard. This is only
+        done if the user clicks after a password is generated
+*/
 function copyToClipboard(){
-    var password = document.getElementById("result").innerHTML;
-    console.log("Generated password " + password)
+
+    var password = document.getElementById('result').innerText;
+
+    if(password != "Select password length and submit") {
+        
+        var textArea = document.createElement('textarea');
+        textArea.value = str;
+        textArea.setAttribute('readonly', '');
+        textArea.style.position = 'absolute';
+        textArea.style.left = '-9999px';
+        
+        // Invisible DOM element is created so that select can be called on it 
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+    }
 }
+
+document.getElementById("submit").addEventListener("click", handleSubmit);
