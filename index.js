0f16fa54912585813d74db8db399331018e33998
samadurm <samadurm@oregonstate.edu>
2020-08-27 18:33:37 -0700

    Added meter to show password strength

--- index.js
+++ index.js
@@ -4,6 +4,23 @@
         the function to generate the random password
 */
 function handleSubmit(){
+
+    const length = validateLength();
+    displayPasswordStrength(length);
+
+    var password = generatePassword(length);
+    estimatePasswordTries(length);
+
+    document.getElementById("result").textContent = password;
+}
+
+/*
+    Function: validateLength
+    Description: Checks if password matches the length bounds
+        set in index.html. If the length is out of allowed bounds,
+        it is set as min or max length accordingly
+*/
+function validateLength(){
     var length = Number(document.getElementById("length").value);
     const maxLen = Number(document.getElementById("length").max);
     const minLen = Number(document.getElementById("length").min);
@@ -15,10 +32,7 @@ function handleSubmit(){
         alert("Below minimum password length. Generating password of " + minLen + " characters.");
         length = minLen;
     }
-
-    var password = generatePassword(length);
-
-    document.getElementById("result").textContent = password;
+    return length;
 }
 
 /*
@@ -41,6 +55,38 @@ function generatePassword(length){
 */
 function randomASCII(min, max){
     return String.fromCharCode(Math.floor(Math.random() * (max - min) + min));
+}
+
+/*
+    Function: estimatePasswordTries
+    Description: Calculates the number of combinations that the password have 
+        given the length
+*/
+function estimatePasswordTries(length){
+    const attemptsText = document.getElementById("numAttempts");
+    var combinations = Math.pow(length, 125-33);
+    attemptsText.innerHTML = "There are <strong>" + combinations + "</strong> password combinations at length " + length + ".";
+}
+
+/*
+    Function: displayPasswordStrength
+    Description: This rates the password strength based on 
+        length, and updates the strength meter
+*/
+function displayPasswordStrength(length){
+    var strength = 0;
+    if(length == 6){
+        strength = 1;
+    } else if(length >= 7 && length < 8){
+        strength = 2;
+    } else if(length >= 8 && length < 10){
+        strength = 5;
+    } else if(length >= 10 && length < 12){
+        strength = 7;
+    } else{
+        strength = 10;
+    }
+    document.getElementById("password-strength").value = strength;
 }
 
 /*
